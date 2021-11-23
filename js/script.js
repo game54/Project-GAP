eb3e1a40d9fc90013d977aa2c97f283fe2c5396a
game54 <ap_nbs@hotmail.com>
2021-11-23 16:17:28 +0200

    up

--- js/script.js
+++ js/script.js
@@ -1,4 +1,23 @@
 const trashIcon = document.querySelector(".trash");
+const menuLi = document.querySelectorAll(".menu__li");
+const gap1Completed = localStorage.getItem("gap1Completed");
+const gap2Completed = localStorage.getItem("gap2Completed");
+const gap3Completed = localStorage.getItem("gap3Completed");
+const gap4Completed = localStorage.getItem("gap4Completed");
+const gap5Completed = localStorage.getItem("gap5Completed");
+const gapsCompleted = [
+  gap1Completed,
+  gap2Completed,
+  gap3Completed,
+  gap4Completed,
+  gap5Completed,
+];
+
+gapsCompleted.forEach(function (gap, i) {
+  if (gap === "true") {
+    menuLi[i].textContent = `Level ${i + 1} ✅`;
+  }
+});
 
 trashIcon.addEventListener("click", function () {
   localStorage.clear();
